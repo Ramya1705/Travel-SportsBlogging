512e9bdd0889825c5c48dc4d23358d372deca80e
Ramya1705 <ramyamusulla@gmail.com>
2025-08-03 15:42:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,6 @@ app.use('/api/upload', uploadRoutes);
 // Error Handler (last middleware)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || '0.0.0.0';
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
